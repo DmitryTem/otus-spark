2e1ab1eb1c5c524b53995b368240ca66b8c3675b
Artem Salnikov <Artem.Y.SALNIKOV@raiffeisen.ru>
2024-11-12 08:47:33 +0200

    Добавлено удаление volumes перед полной сборкой

--- build-cluster.sh
+++ build-cluster.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+docker volume rm hadoop-hive-spark_datanode1 hadoop-hive-spark_datanode2 hadoop-hive-spark_metastore hadoop-hive-spark_namenode
+
+docker build -t hadoop-hive-spark-base ./base
 docker build -t hadoop-hive-spark-master ./master
 docker build -t hadoop-hive-spark-worker ./worker
 docker build -t hadoop-hive-spark-history ./history
